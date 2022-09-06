7afcc46b74b7a1cc991281290426d670c30f0242
KaikyScript <kaikyscript.0day@gmail.com>
2022-09-06 20:15:49 -0300

    fix and Upload new archives

---
+++ aula03/script3_01.js
@@ -0,0 +1,19 @@
+// Declaração de variáveis
+
+let oct = 010;
+let bin = 0b10, hexa = 0x10;
+let dia, nota, cep;
+
+dia = 25;
+nota = 9.7;
+cep = "03257000";
+
+document.write (dia);
+
+document.write("<br>");
+document.write (nota);
+
+document.write("<br>");
+
+document.write(oct,"<br>",bin,"<br>",hexa,"<br>");
+document.write("CEP: " + cep);
